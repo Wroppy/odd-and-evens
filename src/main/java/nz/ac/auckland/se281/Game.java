daf6bcd1be8598b5ce933e8e019aabaa8c749786
Weyman Wong <wxwong806@gmail.com>
2024-05-15 00:14:24 +1200

    Printed the current round at the start of each play

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -5,10 +5,11 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
+  private int round = 1;
 
   /**
    * Given the difficulty, odd or even, and the players name, resets and starts a new game
-   * 
+   *
    * @param difficulty Enum that represents the difficulty levels (EASY, MEDIUM, HARD).
    * @param choice Enum that represents the player choice (Odd or Even).
    * @param options String array containing the player name.
@@ -18,9 +19,16 @@ public class Game {
     String playerName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(playerName);
 
+    this.round = 1;
   }
 
-  public void play() {}
+  /**
+   * Implements the play logic of the game. Prints out the current round and asks the user for their
+   * numeric input
+   */
+  public void play() {
+    MessageCli.START_ROUND.printMessage(String.valueOf(this.round));
+  }
 
   public void endGame() {}
 
