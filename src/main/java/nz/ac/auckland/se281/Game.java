3eec08ddb564221b312e3ea48d9ed87856d8d862
Weyman Wong <wxwong806@gmail.com>
2024-05-15 00:37:10 +1200

    Added getFinger method which gets command line input from a user that is an integer

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -6,9 +6,10 @@ import nz.ac.auckland.se281.Main.Difficulty;
 /** This class represents the Game is the main entry point. */
 public class Game {
   private int round = 1;
+  private String playerName;
 
   /**
-   * Given the difficulty, odd or even, and the players name, resets and starts a new game
+   * Given the difficulty, odd or even, and the players name, resets and starts a new game.
    *
    * @param difficulty Enum that represents the difficulty levels (EASY, MEDIUM, HARD).
    * @param choice Enum that represents the player choice (Odd or Even).
@@ -18,16 +19,45 @@ public class Game {
     // Welcomes the player to the game
     String playerName = options[0];
     MessageCli.WELCOME_PLAYER.printMessage(playerName);
-
+    this.playerName = playerName;
     this.round = 1;
   }
 
   /**
    * Implements the play logic of the game. Prints out the current round and asks the user for their
-   * numeric input
+   * numeric input.
    */
   public void play() {
     MessageCli.START_ROUND.printMessage(String.valueOf(this.round));
+
+    int finger = this.getFinger();
+    MessageCli.PRINT_INFO_HAND.printMessage(this.playerName, String.valueOf(finger));
+
+    this.incrementRound();
+  }
+
+  /**
+   * When called, asks the user for a number that is between 0 and 5 (inclusive) Ensures that the
+   * input is valid and keeps asking until the condition is satisfied.
+   *
+   * @return the user's inputted number from 0 and 5
+   */
+  public int getFinger() {
+    MessageCli.ASK_INPUT.printMessage();
+    String finger = Utils.scanner.nextLine();
+    while (!Utils.isInteger(finger)) {
+      MessageCli.INVALID_INPUT.printMessage();
+      
+      MessageCli.ASK_INPUT.printMessage();
+      finger = Utils.scanner.nextLine();
+    }
+
+    return Integer.parseInt(finger);
+  }
+
+  /** When called, adds 1 to the round class variable */
+  private void incrementRound() {
+    this.round++;
   }
 
   public void endGame() {}
