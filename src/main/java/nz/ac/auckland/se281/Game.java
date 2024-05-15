2c64b91f57a19a0ccfab3f5374ff1088f179c740
Weyman Wong <wxwong806@gmail.com>
2024-05-15 14:32:07 +1200

    Added round outcome message to the play method

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -10,6 +10,7 @@ public class Game {
   private int round = 1;
   private UserPlayer player;
   private AIBot bot;
+  private Choice choice;
 
   /**
    * Given the difficulty, odd or even, and the players name, resets and starts a new game.
@@ -24,8 +25,8 @@ public class Game {
     this.player = new UserPlayer(playerName);
     MessageCli.WELCOME_PLAYER.printMessage(this.player.toString());
     this.round = 1;
-
     this.bot = BotFactory.createBot(difficulty);
+    this.choice = choice;
   }
 
   /**
@@ -43,9 +44,35 @@ public class Game {
     int botFinger = this.bot.getFinger();
     this.printHand(this.bot, botFinger);
 
+    // Prints out who won the round
+    GamePlayer winner = this.didHumanWinRound(finger, botFinger) ? this.player : this.bot;
+    int sum = finger + botFinger;
+    String sumDescription = Utils.isEven(sum) ? "EVEN" : "ODD";
+    MessageCli.PRINT_OUTCOME_ROUND.printMessage(String.valueOf(sum), sumDescription, winner.toString());
+
     this.incrementRound();
   }
 
+  /**
+   * Returns true or false on whether the human player won the round.
+   *
+   * @return whether the human won the round
+   */
+  public boolean didHumanWinRound(int playerFinger, int botFinger) {
+    int sum = playerFinger + botFinger;
+    if (choice == Choice.EVEN) { // User chose even
+      return Utils.isEven(sum);
+    } else { // User chose odd
+      return Utils.isOdd(sum);
+    }
+  }
+
+  /**
+   * Prints out the player's name and hand
+   *
+   * @param gamePlayer
+   * @param finger
+   */
   public void printHand(GamePlayer gamePlayer, int finger) {
     MessageCli.PRINT_INFO_HAND.printMessage(gamePlayer.toString(), String.valueOf(finger));
   }
