0a7a0cc50d4c7635803b56899efcd2d16543e20c
Weyman Wong <wxwong806@gmail.com>
2024-05-15 14:08:14 +1200

    Implemented the easy bot's get finger method

--- src/main/java/nz/ac/auckland/se281/AIBots/EasyBot.java
+++ src/main/java/nz/ac/auckland/se281/AIBots/EasyBot.java
@@ -1,8 +1,10 @@
 package nz.ac.auckland.se281.AIBots;
 
+import nz.ac.auckland.se281.Utils;
+
 public class EasyBot extends AIBot {
   @Override
   public int getFinger() {
-    return 1;
+    return Utils.getRandomNumberRange(0, 5);
   }
 }
